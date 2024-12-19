dbbae9b45e0ab9d1b95c83d84ed453c180c8e67b
csas-cheick <csas.cheick@gmail.com>
2024-12-19 23:12:55 +0100

    modification du responsive

--- Script JS/Script_Accueil.js
+++ Script JS/Script_Accueil.js
@@ -7,28 +7,38 @@ sidebarButtons.forEach((button) => {
     updateDashboardContent(this.id);
   });
 });
+
+// Fonction qui met à jour le contenu du tableau de bord
 function updateDashboardContent(buttonId) {
   const dashboardWindow = document.getElementById("dashboardWindow");
 
   if (buttonId === "GestionVenteBtn") {
     dashboardWindow.innerHTML = `
       <h3 class="text-center">Gestion de ventes</h3>
-      <p class="text-muted text-center">Ici, pour les fonctionnalités de bouton autres !</p>
+      <p class="text-muted text-center">Ici, pour les fonctionnalités de vente !</p>
     `;
   } else if (buttonId === "GestionStockBtn") {
     dashboardWindow.innerHTML = `
       <h3 class="text-center">Gestion de stocks</h3>
-      <p class="text-muted text-center">Ici, pour les fonctionnalités de bouton autres !</p>
+      <p class="text-muted text-center">Ici, pour la gestion des stocks !</p>
     `;
   } else if (buttonId === "GestionCaisseBtn") {
     dashboardWindow.innerHTML = `
       <h3 class="text-center">Gestion de caisse</h3>
-      <p class="text-muted text-center">Ici, pour les fonctionnalités de bouton autres !</p>
+      <p class="text-muted text-center">Ici, pour la gestion de la caisse !</p>
     `;
   } else if (buttonId === "AutresBtn") {
     dashboardWindow.innerHTML = `
       <h3 class="text-center">Autres</h3>
-      <p class="text-muted text-center">Ici, pour les fonctionnalités de bouton autres !</p>
+      <p class="text-muted text-center">Ici, pour d'autres fonctionnalités !</p>
     `;
   }
 }
+
+// Gérer l'ouverture/fermeture du menu pour les petits écrans
+const navbarToggler = document.querySelector(".navbar-toggler");
+const sidebar = document.querySelector(".sidebar");
+
+navbarToggler.addEventListener("click", function () {
+  sidebar.classList.toggle("show");
+});
